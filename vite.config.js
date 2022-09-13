9c74a9a6d6999784e6df8a67a6bb37e70c5e84e8
Safdar Hussain <safdarse063@gmail.com>
2022-09-13 14:10:59 +0500

    deploment

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: `https://github.com/safdar8455/Portfolio/`,
   plugins: [react()]
 })
