3de4d211e9735a4d84f6b35b12e192964de20864
Safdar Hussain <safdarse063@gmail.com>
2022-09-15 14:50:12 +0500

    navbar completed

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,16 +1,32 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom'
+import styled, { ThemeConsumer } from 'styled-components'
 import Navbar from './Navbar'
 
 const Header = () => {
   return (
-    <header>
+    <MainHeader>
       <NavLink to="/">
-        <img src="./images/logo.png" alt="logo" />
+        <img src="./images/logo.png" alt="logo" className='logo' />
       </NavLink>
       <Navbar />
-    </header>
+    </MainHeader>
   )
 }
+
+const MainHeader = styled.header`
+padding: 0 4.8rem;
+height: 10rem;
+background-color: ${({theme})=>theme.colors.bg};
+display: flex;
+justify-content: space-between;
+align-items: center;
+
+.logo{
+  height: auto;
+  max-width: 50%;
+}
+
+`;
 
 export default Header
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,27 +1,63 @@
-import React from 'react'
-import { NavLink } from 'react-router-dom'
+import React from "react";
+import { NavLink } from "react-router-dom";
+import styled from "styled-components";
 
 const Navbar = () => {
   return (
-    <nav>
-      <div className='menueIcon'>
-        <ul className='navbar-list'>
+    <Nav>
+      <div className="menueIcon">
+        <ul className="navbar-list">
           <li>
-            <NavLink to='/Portfolio/'>Home</NavLink>
+            <NavLink to="/Portfolio/" className="navbar-link">
+              Home
+            </NavLink>
           </li>
           <li>
-            <NavLink to='/Portfolio/about'>About</NavLink>
+            <NavLink to="/Portfolio/about" className="navbar-link">
+              About
+            </NavLink>
           </li>
           <li>
-            <NavLink to='/Portfolio/services'>Services</NavLink>
+            <NavLink to="/Portfolio/services" className="navbar-link">
+              Services
+            </NavLink>
           </li>
           <li>
-            <NavLink to='/Portfolio/contact'>Contact</NavLink>
+            <NavLink to="/Portfolio/contact" className="navbar-link">
+              Contact
+            </NavLink>
           </li>
         </ul>
       </div>
-    </nav>
-  )
-}
+    </Nav>
+  );
+};
 
-export default Navbar
+const Nav = styled.nav`
+  .navbar-list {
+    display: flex;
+    gap: 4.8rem;
+  }
+
+  li {
+    list-style: none;
+
+    .navbar-link {
+      &:link,
+      &:visited {
+        display: inline-block;
+        text-decoration: none;
+        font-size: 1.8rem;
+        text-transform: uppercase;
+        color: ${({ theme }) => theme.colors.black};
+        transition: color 0.3s linear;
+      }
+      &:hover,
+      &:active {
+        color: ${({ theme }) => theme.colors.helper};
+      }
+    }
+  }
+`;
+
+export default Navbar;
