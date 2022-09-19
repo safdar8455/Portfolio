6120ccdb68ad7b7194af9dfafc5f7571a2c44569
Safdar Hussain <safdarse063@gmail.com>
2022-09-19 18:21:52 +0500

    set gototop

--- assets/index.8110bc61.js
+++ assets/index.8110bc61.js
@@ -692,7 +692,7 @@ align-items: center;
 @media (max-width: ${({theme:e})=>e.media.mobile}){
     .top-btn {
         right: 0;
-        left: 40%;
+        left: 80%;
     }
 }
 `,G0=()=>V(v0,{theme:{colors:{heading:"rgb(24 24 29)",text:"rgb(24 24 29)",white:"#fff",black:" #212529",helper:"#8490ff",bg:"rgb(249 249 255)",footer_bg:"#0a1435",btn:"rgb(98 84 243)",border:"rgba(98, 84, 243, 0.5)",hr:"#ffffff",gradient:"linear-gradient(0deg, rgb(132 144 255) 0%, rgb(98 189 252) 100%)",shadow:"rgba(0, 0, 0, 0.02) 0px 1px 3px 0px,rgba(27, 31, 35, 0.15) 0px 0px 0px 1px;",shadowSupport:" rgba(0, 0, 0, 0.16) 0px 1px 4px"},media:{mobile:"768px",tab:"998px"}},children:[S(W0,{}),V(y1,{children:[S(H0,{}),V(p1,{children:[S(Cn,{path:"/Portfolio/",element:S(P0,{})}),S(Cn,{path:"/Portfolio/about",element:S(C0,{})}),S(Cn,{path:"/Portfolio/services",element:S(Qd,{})}),S(Cn,{path:"/Portfolio/contact",element:S(Yd,{})}),S(Cn,{path:"/Portfolio/*",element:S(_0,{})})]}),S(Q0,{}),S(M0,{})]})]});Pi.createRoot(document.getElementById("root")).render(S(k0,{children:S(nt.StrictMode,{children:S(G0,{})})}));
